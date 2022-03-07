87b2884d206974b83c01a461e99ed02a1e25f585
notandy82 <notandystanek@gmail.com>
2022-03-07 15:04:43 +0000

    Create welcome function

--- run.py
+++ run.py
@@ -2,5 +2,12 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-username = input("What is your name? ")
-print("Welcome to the Line of Battle, Admiral " + username)
+def welcome_message():
+    """
+    Introduction screen
+    Input field for player to place their name
+    """
+    username = input("What is your name? ")
+    print("Welcome to the Line of Battle, Admiral " + username)
+
+welcome_message()
