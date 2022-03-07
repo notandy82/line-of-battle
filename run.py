7cc89c3aacb5ed6cb959440b9dafb190f7168f54
notandy82 <notandystanek@gmail.com>
2022-03-07 14:14:55 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+username = input("What is your name? ")
+print("Welcome to the Line of Battle, Admiral " + username)
