b8179662cb73cf40a96fc0fd61124d01ce43195b
notandy82 <notandystanek@gmail.com>
2022-03-14 12:59:28 +0000

    Add functions to place ships on player and computer board

--- run.py
+++ run.py
@@ -6,11 +6,25 @@ import time
 from random import randint
 
 # Player and computer boards
-PLAYER_BOARD = [["~"] * 9 for i in range(9)]
-COMPUTER_BOARD = [["~"] * 9 for i in range(9)]
+PLAYER_BOARD = [["\u001b[34m~\u001b[0m"] * 9 for i in range(9)]
+COMPUTER_BOARD = [["\u001b[34m~\u001b[0m"] * 9 for i in range(9)]
 
 # List of ship lengths for the game
 SHIPS = [2, 3, 3, 4, 5]
+
+# Letter to number conversion for coordinate system
+convert_letters = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7, "I": 8}
+
+def print_board(board):
+    """
+    Creates the boards for the player and computer
+    """
+    print("  A B C D E F J H I")
+    print("  ~~~~~~~~~~~~~~~~~")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 
 def welcome_message():
@@ -42,7 +56,7 @@ def welcome_message():
     print("You have 1 gunboat that covers 2 spaces")
     print("Alternate turns with the computer firing shots")
     print("A hit is shown with \u001b[31mX\u001b[0m")
-    print("A miss is shown with \u001b[34mO\u001b[0m")
+    print("A miss is shown with O")
     print("You must sink the computer's vessels before yours are sunk")
     time.sleep(5)
 
@@ -72,7 +86,33 @@ def name_check(username):
         return True
 
 
-welcome_message()
-username_input()
+def place_computer_ships(COMPUTER_BOARD):
+    """
+    Random placement for the computer's ships
+    """
+    for ship in range(5):
+        ship_column, ship_row = randint(0,9), randint(0,9)
+        while board[ship_column][ship_row] == "X":
+            ship_column, ship_row = randint(0,9), randint(0,9)
+        board[ship_column][ship_row] = "X"
+
+
+def place_ships(PLAYER_BOARD):
+    """
+    Function for player to place ships
+    """
+    column = input("Choose a column A - I to place your ship")
+    while column not in "ABCDEFGHI":
+        print("Please enter a valid column")
+        column = input("Choose a column A - I to place your ship")
+    row = input("Choose a row 1 - 9 to place your ship")
+    while row not in "123456789":
+        print("Please enter a valid row")
+        row = input("Choose a row 1 - 9 to place your ship")
+
+# welcome_message()
+# username_input()
 # place_ships()
 # place_computer_ships()
+print_board(COMPUTER_BOARD)
+print_board(PLAYER_BOARD)
